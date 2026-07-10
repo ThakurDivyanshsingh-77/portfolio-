2362ec926dbca4435ad61c29000e6920e56bd9e1
ThakurDivyanshsingh-77 <divyanshthakur.2251@gmail.com>
2026-07-10 10:48:56 +0530

    Updated project

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ const corsOptions = {
 
 // Middleware
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
